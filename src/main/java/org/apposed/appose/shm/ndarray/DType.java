eedde86e806489c2992411d4a7467db15310594d
tpietzsch <tobias.pietzsch@gmail.com>
2024-04-22 11:05:59 +0200

    formatting

--- src/main/java/org/apposed/appose/shm/ndarray/DType.java
+++ src/main/java/org/apposed/appose/shm/ndarray/DType.java
@@ -1,26 +1,25 @@
 package org.apposed.appose.shm.ndarray;
 
-public enum DType
-{
-	INT8( "int8", Byte.BYTES ),
-	INT16( "int16", Short.BYTES ),
-	INT32( "int32", Integer.BYTES ),
-	INT64( "int64", Long.BYTES ),
-	UINT8( "uint8", Byte.BYTES ),
-	UINT16( "uint16", Short.BYTES ),
-	UINT32( "uint32", Integer.BYTES ),
-	UINT64( "uint64", Long.BYTES ),
-	FLOAT32( "float32", Float.BYTES ),
-	FLOAT64( "float64", Double.BYTES ),
-	COMPLEX64( "complex64", Float.BYTES * 2 ),
-	COMPLEX128( "complex128", Double.BYTES * 2 ),
-	BOOL( "bool", 1 );
+public enum DType {
+	INT8("int8", Byte.BYTES), //
+	INT16("int16", Short.BYTES), //
+	INT32("int32", Integer.BYTES), //
+	INT64("int64", Long.BYTES), //
+	UINT8("uint8", Byte.BYTES), //
+	UINT16("uint16", Short.BYTES), //
+	UINT32("uint32", Integer.BYTES), //
+	UINT64("uint64", Long.BYTES), //
+	FLOAT32("float32", Float.BYTES), //
+	FLOAT64("float64", Double.BYTES), //
+	COMPLEX64("complex64", Float.BYTES * 2), //
+	COMPLEX128("complex128", Double.BYTES * 2), //
+	BOOL("bool", 1);
 
 	private final String label;
 
 	private final int bytesPerElement;
 
-	DType( final String label, final int bytesPerElement )
+	DType(final String label, final int bytesPerElement)
 	{
 		this.label = label;
 		this.bytesPerElement = bytesPerElement;
@@ -36,46 +35,43 @@ public enum DType
 		return bytesPerElement;
 	}
 
-	// --- maybe ? --
-
 	public String json()
 	{
 		return label();
 	}
 
-	public static DType fromJson( final String json ) throws IllegalArgumentException
+	public static DType fromJson(final String json) throws IllegalArgumentException
 	{
-//		return valueOf( json.toUpperCase() );
-		switch ( json )
-		{
-		case "int8":
-			return INT8;
-		case "int16":
-			return INT16;
-		case "int32":
-			return INT32;
-		case "int64":
-			return INT64;
-		case "uint8":
-			return UINT8;
-		case "uint16":
-			return UINT16;
-		case "uint32":
-			return UINT32;
-		case "uint64":
-			return UINT64;
-		case "float32":
-			return FLOAT32;
-		case "float64":
-			return FLOAT64;
-		case "complex64":
-			return COMPLEX64;
-		case "complex128":
-			return COMPLEX128;
-		case "bool":
-			return BOOL;
-		default:
-			throw new IllegalArgumentException();
+		// return valueOf( json.toUpperCase() );
+		switch (json) {
+			case "int8":
+				return INT8;
+			case "int16":
+				return INT16;
+			case "int32":
+				return INT32;
+			case "int64":
+				return INT64;
+			case "uint8":
+				return UINT8;
+			case "uint16":
+				return UINT16;
+			case "uint32":
+				return UINT32;
+			case "uint64":
+				return UINT64;
+			case "float32":
+				return FLOAT32;
+			case "float64":
+				return FLOAT64;
+			case "complex64":
+				return COMPLEX64;
+			case "complex128":
+				return COMPLEX128;
+			case "bool":
+				return BOOL;
+			default:
+				throw new IllegalArgumentException();
 		}
 	}
 }
--- src/main/java/org/apposed/appose/shm/ndarray/Shape.java
+++ src/main/java/org/apposed/appose/shm/ndarray/Shape.java
@@ -2,84 +2,74 @@ package org.apposed.appose.shm.ndarray;
 
 import java.util.Arrays;
 
-public class Shape
-{
-	public enum Order
-	{
-		C_ORDER,
-		F_ORDER;
-	}
+public class Shape {
+
+	public enum Order {C_ORDER, F_ORDER}
 
 	private final Order order;
-
 	private final int[] shape;
 
-	public Shape( final Order order, final int... shape )
-	{
+	public Shape(final Order order, final int... shape) {
 		this.order = order;
 		this.shape = shape;
 	}
 
-	public int get( final int d )
-	{
-		return shape[ d ];
+	public int get(final int d) {
+		return shape[d];
 	}
 
-	public int length()
-	{
+	public int length() {
 		return shape.length;
 	}
 
-	public Order order()
-	{
+	public Order order() {
 		return order;
 	}
 
-	public long numElements()
-	{
+	public long numElements() {
 		long n = 1;
-		for ( int s : shape )
+		for (int s : shape) {
 			n *= s;
+		}
 		return n;
 	}
 
-	public int[] toIntArray( final Order order )
-	{
-		if ( order.equals( this.order ) )
+	public int[] toIntArray() {
+		return shape;
+	}
+
+	public int[] toIntArray(final Order order) {
+		if (order.equals(this.order)) {
 			return shape;
-		else
-		{
-			final int[] ishape = new int[ shape.length ];
-			Arrays.setAll( ishape, i -> shape[ shape.length - i - 1 ] );
+		}
+		else {
+			final int[] ishape = new int[shape.length];
+			Arrays.setAll(ishape, i -> shape[shape.length - i - 1]);
 			return ishape;
 		}
 	}
 
-	public long[] toLongArray( final Order order )
-	{
-		final long[] lshape = new long[ shape.length ];
-		if ( order.equals( this.order ) )
-			Arrays.setAll( lshape, i -> shape[ i ] );
-		else
-			Arrays.setAll( lshape, i -> shape[ shape.length - i - 1 ] );
+	public long[] toLongArray() {
+		return toLongArray(order);
+	}
+
+	public long[] toLongArray(final Order order) {
+		final long[] lshape = new long[shape.length];
+		if (order.equals(this.order)) {
+			Arrays.setAll(lshape, i -> shape[i]);
+		}
+		else {
+			Arrays.setAll(lshape, i -> shape[shape.length - i - 1]);
+		}
 		return lshape;
 	}
 
-	public long[] toLongArray()
-	{
-		return toLongArray( order );
-	}
-
-	public int[] toIntArray()
-	{
-		return shape;
-	}
-
-	public Shape to( final Order order )
-	{
-		if ( order.equals( this.order ) )
+	public Shape to(final Order order) {
+		if (order.equals(this.order)) {
 			return this;
-		else
-			return new Shape( order, toIntArray( order ) );
+		}
+		else {
+			return new Shape(order, toIntArray(order));
+		}
 	}
 }
