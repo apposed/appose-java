4f1fb9cc130c243998c726a02b5756cf52f38fe8
Curtis Rueden <ctrueden@wisc.edu>
2024-09-26 14:13:25 -0500

    CI: deploy builds only on Linux
    We don't want the multiple CI nodes to clash.

--- .github/setup.sh
+++ .github/setup.sh
@@ -5,3 +5,10 @@ sh ci-setup-github-actions.sh
 # Install latest development version of appose-python.
 echo "==> Installing appose-python..."
 python -m pip install git+https://github.com/apposed/appose-python.git#egg=appose
+
+# Let the Linux build handle artifact deployment.
+if [ "$(uname)" != Linux ]
+then
+  echo "No deploy -- non-Linux build"
+  echo "NO_DEPLOY=1" >> $GITHUB_ENV
+fi
