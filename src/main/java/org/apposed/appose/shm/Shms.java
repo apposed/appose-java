661458bcc53a8af8f0433232ccee1aaa02f0aad8
Curtis Rueden <ctrueden@wisc.edu>
2025-10-23 09:02:48 -0500

    Tidy up the Shms utility class
    * Add author tags.
    * Group constants together, methods together, as per usual.
    * Add missing javadoc.
    * Change package-private methods to public, for extensibility.
    
    Without the public keyword, no other ShmFactory implementations
    (outside of code in this same package) could make use of the utility
    methods, defeating the purpose of ShmFactory being plugin-driven.

--- src/main/java/org/apposed/appose/shm/Shms.java
+++ src/main/java/org/apposed/appose/shm/Shms.java
@@ -36,6 +36,9 @@ import java.util.Random;
 
 /**
  * Utilities used in platform-specific {@code ShmBase} implementations.
+ *
+ * @author Tobias Pietzsch
+ * @author Curtis Rueden
  */
 public final class Shms {
 
@@ -45,67 +48,53 @@ public final class Shms {
 
 	/**
 	 * Constant to specify that the shared memory segment that is going to be
-	 * open is only for reading
+	 * open is only for reading.
 	 */
-	static int O_RDONLY = 0;
+	public static int O_RDONLY = 0;
 
 	/**
 	 * Constant to specify that the shared memory segment that is going to be
-	 * open is for reading and/or writing
+	 * open is for reading and/or writing.
 	 */
-	static int O_RDWR = 2;
+	public static int O_RDWR = 2;
 
 	/**
 	 * Constant to specify that the shared memory segment that is going to be
-	 * open will be created if it does not exist
+	 * open will be created if it does not exist.
 	 */
-	static int O_CREAT = 64;
+	public static int O_CREAT = 64;
 
 	/**
 	 * Constant to specify that the shared memory regions mapped can be read but
-	 * not written
+	 * not written.
 	 */
-	static int PROT_READ = 0x1;
+	public static int PROT_READ = 0x1;
 
 	/**
-	 * Constant to specify that the shared memory regions mapped can be written
+	 * Constant to specify that the shared memory regions mapped can be written.
 	 */
-	static int PROT_WRITE = 0x2;
+	public static int PROT_WRITE = 0x2;
 
 	/**
 	 * Constant to specify that the shared memory regions mapped can be shared
-	 * with other processes
+	 * with other processes.
 	 */
-	static int MAP_SHARED = 0x01;
-
-	/**
-	 * Add leading slash if {@code name} doesn't have one.
-	 */
-	static String withLeadingSlash(String name) {
-		return name.startsWith("/") ? name : ("/" + name);
-	}
-
-	/**
-	 * Remove leading slash if {@code name} has one.
-	 */
-	static String withoutLeadingSlash(String name) {
-		return name.startsWith("/") ? name.substring(1) : name;
-	}
+	public static int MAP_SHARED = 0x01;
 
 	/**
 	 * FreeBSD (and perhaps other BSDs) limit names to 14 characters.
 	 */
-	static final int SHM_SAFE_NAME_LENGTH = 14;
+	public static final int SHM_SAFE_NAME_LENGTH = 14;
 
 	/**
 	 * Shared memory block name prefix for Mac and Linux
 	 */
-	static final String SHM_NAME_PREFIX_POSIX = "/psm_";
+	public static final String SHM_NAME_PREFIX_POSIX = "/psm_";
 
 	/**
 	 * Shared memory block name prefix for Windows
 	 */
-	static final String SHM_NAME_PREFIX_WIN = "wnsm_";
+	public static final String SHM_NAME_PREFIX_WIN = "wnsm_";
 
 	/**
 	 * Creates a shared memory block by utilizing the appropriate
@@ -125,13 +114,27 @@ public final class Shms {
 	}
 
 	/**
+	 * Add leading slash if {@code name} doesn't have one.
+	 */
+	public static String withLeadingSlash(String name) {
+		return name.startsWith("/") ? name : ("/" + name);
+	}
+
+	/**
+	 * Remove leading slash if {@code name} has one.
+	 */
+	public static String withoutLeadingSlash(String name) {
+		return name.startsWith("/") ? name.substring(1) : name;
+	}
+
+	/**
 	 * Creates a random filename for the shared memory object.
 	 *
 	 * @param maxLength maximum length of the generated name
 	 * @param prefix    prefix of the generated filename
 	 * @return a random filename with the given {@code prefix}.
 	 */
-	static String makeFilename(int maxLength, String prefix) {
+	public static String makeFilename(int maxLength, String prefix) {
 		// number of random bytes to use for name
 		final int nbytes = (maxLength - prefix.length()) / 2;
 		if (nbytes < 2) {
@@ -142,7 +145,17 @@ public final class Shms {
 		return name;
 	}
 
-	static void checkSize(String shmName, long prevSize, long size) {
+	/**
+	 * Validates the size of an already-existing shared memory block.
+	 *
+	 * @param shmName Name of the shared memory block to validate.
+	 * @param prevSize Previous/current size of the shared memory block.
+	 * @param size Proposed revised size of the shared memory block.
+	 *
+	 * @throws RuntimeException
+	 *     If the shared memory block already exists with smaller size than the one given.
+	 */
+	public static void checkSize(String shmName, long prevSize, long size) {
 		final boolean alreadyExists = prevSize >= 0;
 		if (alreadyExists && prevSize < size) {
 			throw new RuntimeException("Shared memory segment '" + shmName + "' already exists with smaller size. "
