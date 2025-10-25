53a021ff26b34f4f4ae40a43b391ce6d5e7d68c8
Curtis Rueden <ctrueden@wisc.edu>
2025-10-24 20:40:43 -0500

    CI: print information about external builder tools

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
 sh ci-build.sh
+result=$?
+
+echo
+echo "===== uv ====="
+~/.local/share/appose/.uv/bin/uv self version
+~/.local/share/appose/.uv/bin/uv cache dir
+echo
+echo "===== pixi ====="
+~/.local/share/appose/.pixi/bin/pixi info
+echo
+echo "===== mamba ====="
+~/.local/share/appose/.mamba/bin/micromamba info
+
+exit $result
