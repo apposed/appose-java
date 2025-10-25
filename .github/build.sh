795e57f88f3e0be0c66e763265511134d31c301c
Curtis Rueden <ctrueden@wisc.edu>
2025-10-25 16:00:22 -0500

    CI: further improve tool cache caching

--- .github/build.sh
+++ .github/build.sh
@@ -9,30 +9,33 @@ case "$(uname)" in
 esac
 
 uv=$(find "$HOME/.local/share/appose" -name uv$EXE -type f | head -n1)
-if [ "$uv" ]; then
+if [ "$uv" ]; then (
   echo
   echo "===== uv ====="
+  set -x
   "$uv" --version
   "$uv" cache dir
-else
+) else
   echo "No uv executable found!"
 fi
 
 pixi=$(find "$HOME/.local/share/appose" -name pixi$EXE -type f | head -n1)
-if [ "$pixi" ]; then
+if [ "$pixi" ]; then (
   echo
   echo "===== pixi ====="
+  set -x
   "$pixi" info
-else
+) else
   echo "No pixi executable found!"
 fi
 
 mamba=$(find "$HOME/.local/share/appose" -name micromamba$EXE -type f | head -n1)
-if [ "$mamba" ]; then
+if [ "$mamba" ]; then (
   echo
   echo "===== mamba ====="
+  set -x
   "$mamba" info
-else
+) else
   echo "No micromamba executable found!"
 fi
 
